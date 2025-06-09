935c2946cb1c6d168e7036a9a55deb5d9d97dab0
Gus <ayehif@gmail.com>
2025-06-09 08:29:45 +0200

    y

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
 from flask import Flask, request
+import requests
 app = Flask(__name__)
 
 @app.route('/get_source/', methods=["GET","POST"])
 def get_source():
     action = request.args.get("action") 
     if action:
-        return 'Hello World!'
+        zapier_result='hey there!'
+        data = {
+            'subject': 'Your articles are here!',
+            'contents': zapier_result
+        }
 
-if __name__ == '__main__':
-    app.run()
+        requests.post('https://hooks.zapier.com/hooks/catch/5743071/uy49cx4/', json=data)
+        return 'Sent Zap'
+    else:
+        return 'No data provided'
+    
+@app.route('/analyze/', methods=["POST"])
+def analyze():
+    source=request.form.get("source")
+    if source:
+        return "Got it"
+    else:
+        return 'No data provided'
+    
+    
+
 
